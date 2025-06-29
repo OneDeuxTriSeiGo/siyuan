b5825d73db544f45b995ddce6990ae61313abffc
Jacob Abel <jacobabel@nullpo.dev>
2025-06-28 21:34:00 -0400

    Remove Cloud Account Code from Kernel
    Removes cloud/paid account support but does not enable paid for
    features and does not attempt to bypass the paywall.
    
    Signed-off-by: Jacob Abel <jacobabel@nullpo.dev>

--- kernel/api/system.go
+++ kernel/api/system.go
@@ -275,7 +275,6 @@ func exportConf(c *gin.Context) {
 		clonedConf.Export.PandocBin = ""
 	}
 	clonedConf.UserData = ""
-	clonedConf.Account = nil
 	clonedConf.AccessAuthCode = ""
 	if nil != clonedConf.System {
 		clonedConf.System.ID = ""
